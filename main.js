56cde1be9ff37a6b779b7f02e37cbfed48a05a41
Jackie Dallas <jackie.r.dallas@gmail.com>
2021-10-16 23:28:31 -0500

    put glitch movie fetch into a function

--- main.js
+++ main.js
@@ -1,9 +1,11 @@
 const omdbKey = 'e2480ab6'
 
+function getGlitchMovies() {
+	fetch('https://codeup-json-server.glitch.me/movies')
+		.then((response) => response.json())
+		.then((jsonData) => console.log(jsonData))
+}
 
-fetch('https://codeup-json-server.glitch.me/movies')
-	.then((response) => response.json())
-	.then((jsonData) => console.log(jsonData))
 
 fetch('http://www.omdbapi.com/?i=tt3896198&apikey=e2480ab6')
 	.then((response) => response.json())
