d4528fc6d3369d01f4f527beb23f8637a91d1c09
Jackie Dallas <jackie.r.dallas@gmail.com>
2021-10-17 21:28:32 -0500

    fixed our url for the fetch and added it to a const

--- main.js
+++ main.js
@@ -1,12 +1,14 @@
 const omdbKey = 'e2480ab6'
 
-function getGlitchMovies() {
-	fetch('https://codeup-json-server.glitch.me/movies')
+const getGlitchMovies = fetch('https://adamina-jackie-cinema.glitch.me/movies');
+
+
+// fetch('http://www.omdbapi.com/?i=tt3896198&apikey=e2480ab6')
+// 	.then((response) => response.json())
+// 	.then((jsonData) => console.log(jsonData))
+
+$(document).ready(function () {
+	getGlitchMovies
 		.then((response) => response.json())
-		.then((jsonData) => console.log(jsonData))
-}
-
-
-fetch('http://www.omdbapi.com/?i=tt3896198&apikey=e2480ab6')
-	.then((response) => response.json())
-	.then((jsonData) => console.log(jsonData))
+		.then((movies) => console.log(movies))
+})
